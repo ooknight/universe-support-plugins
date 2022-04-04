2267f87398a078ed6d18bd1fcd4f79a3463ccdb6
孙雷 <sunl@rsckj.com>
2022-04-04 15:05:11 +0800

    update

--- universe-prime/src/main/java/io/github/ooknight/universe/core/prime/constant/AspectOrder.java
+++ universe-prime/src/main/java/io/github/ooknight/universe/core/prime/constant/AspectOrder.java
@@ -2,12 +2,14 @@ package io.github.ooknight.universe.core.prime.constant;
 
 public final class AspectOrder {
 
-    public static final int RETRY = 101;
-    public static final int TRACE = 102;
-    public static final int MONITOR = 103;
-    public static final int LOCK = 104;
-    public static final int CACHE = 105;
-    public static final int TRANSACTION = 106;
+    /* CONTROLLER */
+    public static final int TRACE = 101;
+    public static final int MONITOR = 102;
+    /* SERVICE */
+    public static final int RETRY = 201;
+    public static final int LOCK = 202;
+    public static final int CACHE = 203;
+    public static final int TRANSACTION = 204;
 
     private AspectOrder() {}
 
